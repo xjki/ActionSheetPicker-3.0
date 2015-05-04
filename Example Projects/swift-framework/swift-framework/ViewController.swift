b804ed20c7832d0b9d7828d33bd7d71f4b37806d
Petr Korolev <sky4winder@gmail.com>
2015-05-04 14:28:03 +0300

    add project

---
+++ Example Projects/swift-framework/swift-framework/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  swift-framework
+//
+//  Created by Petr Korolev on 04/05/15.
+//  Copyright (c) 2015 Petr Korolev. All rights reserved.
+//
+
+import UIKit
+import CoreActionSheetPicker
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnClick(sender: AnyObject) {
+       ActionSheetDatePicker
+    }
+
+}
+
