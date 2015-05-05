00af0a63765fd48ae5f65cbf41a050cbb3b534e2
Petr Korolev <sky4winder@gmail.com>
2015-05-05 17:36:00 +0300

    add swift example

---
+++ Example Projects/swift-pods/swift-pods/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift-pods
+//
+//  Created by Petr Korolev on 05/05/15.
+//  Copyright (c) 2015 Petr Korolev. All rights reserved.
+//
+
+import UIKit
+import ActionSheetPicker_3_0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
