13e883703212097f985f21ed91b2cac182b8924f
Petr Korolev <sky4winder@gmail.com>
2020-02-16 04:25:08 +0700

    recreate CoreActionSheetPicker

---
+++ CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker/CoreActionSheetPicker.h
@@ -0,0 +1,19 @@
+//
+//  CoreActionSheetPicker.h
+//  CoreActionSheetPicker
+//
+//  Created by Petr Korolev on 16.02.2020.
+//  Copyright © 2020 Petr Korolev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreActionSheetPicker.
+FOUNDATION_EXPORT double CoreActionSheetPickerVersionNumber;
+
+//! Project version string for CoreActionSheetPicker.
+FOUNDATION_EXPORT const unsigned char CoreActionSheetPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreActionSheetPicker/PublicHeader.h>
+
+#import <CoreActionSheetPicker/ActionSheetStringPicker.h>
